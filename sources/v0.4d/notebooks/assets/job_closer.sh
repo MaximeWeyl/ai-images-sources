dc3e7c042e062e5e9e1f49951155109c08b7cca4
maxime.weyl <maxime.weyl@ovhcloud.com>
2021-12-27 12:04:36 +0000

    updated version v0.4d using ribs : added 130 files

---
+++ sources/v0.4d/notebooks/assets/job_closer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script should be called by your job entrypoint
+# It allows you to exit the job from the inside, with the exit_job script
+
+while true ; do
+  if [ -f /tmp/.job_closer ]; then
+    exit_status="$(cat /tmp/.job_closer)"
+    echo "Exiting the job with status $exit_status"
+    rm -f /tmp/.job_closer
+    exit "$exit_status"
+  fi
+done
